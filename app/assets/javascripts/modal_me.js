1541342a5fb488666ea7c0c5869f6b191ed70fa4
Calvin Tee <calvin@collectskin.com>
2015-07-03 14:22:39 +0800

    modalme

--- app/assets/javascripts/modal_me.js
+++ app/assets/javascripts/modal_me.js
@@ -3,6 +3,7 @@ jQuery(function(){
   jQuery('a[data-modalme-url*=]').click(function(){
     jQuery(this).modal('show');
     jQuery('.modal-title').html($(this).data('modalme-title'));
+    jQuery('.modal-footer').html($(this).data('modalme-footer'));
     jQuery.ajax({url: $(this).data('modalme-url'),
     success: function(data){
       jQuery('.modal-body').html(data);
